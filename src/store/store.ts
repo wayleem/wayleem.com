32de10c1de0f567463323bbcd382083d0b9dfb07
William Huang <wayleemh@gmail.com>
2023-08-13 15:58:45 -0400

    setup

---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { createReducer, configureStore } from '@reduxjs/toolkit'
+/*
+export interface RootState {}
+
+const rootReducer = createReducer()
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+*/
