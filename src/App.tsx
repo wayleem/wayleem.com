fae805def914dcb104b5379fb680589e006d68f9
William Huang <wayleemh@gmail.com>
2023-08-15 19:46:57 -0400

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Skills from './pages/Skills'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="*" element={<Layout />}></Route>
+        <Route path="*" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
       </Routes>
     </HashRouter>
   )
