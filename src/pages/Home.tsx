32711527542152d04b5ab8b436880c518aa1275d
William Huang <wayleemh@gmail.com>
2023-08-16 03:16:46 -0400

    add home content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>Home</div>
+  return (
+    <div className="text-center">
+      <h1 className="h1 mt-[10%]">Welcome</h1>
+      <h2 className="subtitle-text mt-[25%]">
+        You can rotate the cube by dragging
+      </h2>
+    </div>
+  )
 }
 
 export default Home
