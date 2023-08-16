f923307da3fb46e07d656c727fd76e9eebce0235
William Huang <wayleemh@gmail.com>
2023-08-16 03:15:27 -0400

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
 import Contact from './pages/Contact'
+import About from './pages/About'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="*" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
           <Route path="skills" element={<Skills />} />
           <Route path="contact" element={<Contact />} />
