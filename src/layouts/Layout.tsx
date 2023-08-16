a8260c926e4f9197feb401bf3e6863836c245319
William Huang <wayleemh@gmail.com>
2023-08-16 03:16:01 -0400

    add layout content

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, NavLink } from 'react-router-dom'
 import CubeScene from './CubeScene'
 
 function Layout() {
+  const location = useLocation()
+
   return (
     <>
       <CubeScene />
-      <div className="absolute left-0 top-0 z-10 text-white">
+      <div className="absolute p-10 left-0 top-0 w-full h-full z-10 pointer-events-none">
         <Outlet />
+      </div>
+      <div className="flex flex-row absolute right-0 top-0 z-20 mt-4 mr-4">
+        <NavLink
+          className="font-subtitle text-xl text-base-content font-semibold hover:text-base-100 hover:underline"
+          to="/"
+        >
+          home
+        </NavLink>
+        <h1 className="subtitle-text">{location.pathname}</h1>
       </div>
     </>
   )
