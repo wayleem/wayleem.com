393a26d714b6a400bd2d08649ab74d5e6201a0fb
William Huang <wayleemh@gmail.com>
2023-08-16 03:16:58 -0400

    add about content

---
+++ src/pages/About.tsx
@@ -0,0 +1,43 @@
+function About() {
+  return (
+    <div className="ml-12 mt-12">
+      <div className="flex-col">
+        <h1 className="h1">Hello,</h1>
+        <h1 className="h1 mt-8">I'm William</h1>
+        <h2 className="body mt-16">I'm currently a student at SBU</h2>
+        <h2 className="body mt-8 w-[35%]">
+          I am completing a B.S. degree in Technical Systems Management. I have
+          taken several programming courses at SBU that had enriched my
+          concurrent skillset in programing. I am autodidactive by nature as I
+          love learning to use the latest, most prominent technologies, and
+          frameworks. There are often cases where I feel that SBU didn't delve
+          deeper into my interests, and thats where I go push myself to really
+          chase after my dreams.
+        </h2>
+        <h2 className="body mt-8 w-[35%]">
+          I love learning about proper practice. I would sometimes go back where
+          I started to retrace my footsteps to really clean up bad habits, and
+          pick up good ones. There are many experiences where I learned to be
+          humble, and be the student to cultivate myself.
+        </h2>
+        <h2 className="body mt-8 w-[35%]">
+          There are also times where I know I must take the lead. I have no
+          problem showing initiative in areas that I am confident in, and areas
+          that I am new to.
+        </h2>
+        <h2 className="body mt-8 w-[35%]">
+          I am experienced in Front-End development and Game development. I am
+          always open to go outside my comfort zone and pick up new experiences
+          to broaden my horizons.
+        </h2>
+      </div>
+      <h3 className="float-text-lg right-[30%] top-[5%]">Front-End;</h3>
+      <h3 className="float-text-lg right-[10%] top-[40%]">Developer;</h3>
+      <h3 className="float-text-lg right-[20%] bottom-[20%]">
+        Software Engineer;
+      </h3>
+    </div>
+  )
+}
+
+export default About
