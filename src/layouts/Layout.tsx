de44355f68a397c016c08c73b1d7b44b242ec855
william <wayleemh@gmail.com>
2023-09-11 02:53:06 -0400

    add mobile response to cubescene

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,15 +5,23 @@ import CubeScene from './CubeScene'
 function Layout() {
   const location = useLocation()
   const outlet = useOutlet()
+  const isHomePage = location.pathname === '/'
 
   return (
     <>
       {/* threejs canvas */}
-      <div className="absolute inset-0 w-full h-full">
+      <div
+        className={`absolute inset-0 w-full h-full ${
+          isHomePage ? 'block' : 'hidden md:block'
+        }`}
+      >
         <CubeScene />
       </div>
       {/* page content */}
-      <div className="absolute inset-0 w-full h-full z-10 overflow-hidden select-none pointer-events-none">
+      <div
+        className={`absolute inset-0 w-full h-full z-10 overflow-hidden select-none pointer-events-none 
+  ${isHomePage ? 'bg-transparent' : 'bg-neutral md:bg-transparent'}`}
+      >
         <AnimatePresence mode="wait">
           <motion.main key={location.pathname}>{outlet}</motion.main>
         </AnimatePresence>
@@ -31,7 +39,7 @@ function Layout() {
           exit={{ opacity: 0 }}
         >
           <NavLink
-            className="font-subtitle text-xl text-base-content font-semibold hover:text-base-100 hover:underline underline-offset-2"
+            className="font-subtitle md:text-xl text-base-content font-semibold hover:text-base-100 hover:underline underline-offset-2"
             to="/"
           >
             home
@@ -39,7 +47,7 @@ function Layout() {
           <span className="subtitle-text">{location.pathname}</span>
         </motion.div>
       </AnimatePresence>
-      <h1 className="absolute mb-2 ml-2 font-subtitle text-sm left-0 bottom-0 z-20">
+      <h1 className="absolute text-center md:text-start mb-2 md:ml-2 font-subtitle text-xs md:text-sm inset-x-0 md:left-0 bottom-0 z-20">
         Copyright © 2023 William Huang.
       </h1>
     </>
