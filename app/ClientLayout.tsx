4bbcda6160b5b5ef58563172ee9dfcb7bcae096a
william <wayleemh@gmail.com>
2024-08-12 01:32:49 -0400

    setup nextjs layout

---
+++ app/ClientLayout.tsx
@@ -0,0 +1,40 @@
+// app/ClientLayout.tsx
+"use client";
+import Navbar from "./components/Navbar";
+import { useRef } from "react";
+import Home from "./page";
+import About from "./about/page";
+import Skills from "./skills/page";
+import Projects from "./projects/page";
+import Articles from "./articles/page";
+import Contact from "./contact/page";
+import { useScrollToSection } from "./hooks/useScrollToSection";
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+	useScrollToSection();
+	const mainRef = useRef<HTMLElement>(null);
+	return (
+		<body className="font-body w-full min-h-screen">
+			<Navbar scrollRef={mainRef} />
+			<main ref={mainRef} className="relative">
+				<section id="home" className="min-h-screen w-full bg-white">
+					<Home />
+				</section>
+				<section id="about" className="min-h-screen w-full bg-[#D1EAFF]">
+					<About />
+				</section>
+				<section id="skills" className="min-h-screen w-full bg-[#FFFFD1]">
+					<Skills />
+				</section>
+				<section id="projects" className="min-h-screen w-full bg-[#ffd4a6]">
+					<Projects />
+				</section>
+				<section id="articles" className="min-h-screen w-full bg-[#ff99a6]">
+					<Articles />
+				</section>
+				<section id="contact" className="min-h-screen w-full bg-[#EACFFF]">
+					<Contact />
+				</section>
+			</main>
+		</body>
+	);
+}
