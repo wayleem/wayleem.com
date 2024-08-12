fdda7d2c8190fdf9cc2b23aa38a95d74f94d3f38
william <wayleemh@gmail.com>
2024-08-12 01:30:43 -0400

    set tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { colors } from "./app/static/colors";
 
 const config: Config = {
 	content: [
@@ -7,8 +8,52 @@ const config: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				title: ["var(--font-poppins)"],
+				body: ["var(--font-inter)"],
+				nav: ["var(--font-work-sans)"],
+			},
+			colors: {
+				green: {
+					primary: "#BAFFC9",
+					secondary: "#D1FFD9",
+					background: "#E8FFEC",
+				},
+				blue: {
+					primary: "#BAE1FF",
+					secondary: "#D1EAFF",
+					background: "#E8F4FF",
+				},
+				yellow: {
+					primary: "#FFFFBA",
+					secondary: "#FFFFD1",
+					background: "#FFFFE8",
+				},
+				orange: {
+					primary: "#FFDFBA",
+					secondary: "#ffd4a6",
+					background: "#FFF4E8",
+				},
+				pink: {
+					primary: "#FFB3BA",
+					secondary: "#ff99a6",
+					background: "#FFE8EC",
+				},
+				purple: {
+					primary: "#E1BAFF",
+					secondary: "#EACFFF",
+					background: "#F4E8FF",
+				},
+			},
+			screens: {
+				"3xl": "1600px", // Very large desktops
+				"4xl": "1920px", // Ultra-wide screens
+				"5xl": "2560px", // Beyond ultra-wide screens
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
+
 export default config;
